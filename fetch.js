e0949028a29cfac71c095592b2d57aef32ff53ac
kiranbinoy <kiranbinoy257@gmail.com>
2024-05-09 07:28:56 +0530

    main

--- fetch.js
+++ fetch.js
@@ -6,14 +6,28 @@
 
 // FETCH BUTTON
 
+// function getData(){
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res)=>{return res.json()})
+//     .then((data)=>{
+//         console.log(data);
+//         str=``
+//         data.map((post)=>{
+//             str+=`<li>${post.id}->${post.title}->${post.body}</li>`
+//         })
+//         document.getElementById("list").innerHTML=str
+//     })
+//     .catch((error)=>{console.log(error);})
+// }
+
 function getData(){
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch("https://jsonplaceholder.typicode.com/todos")
     .then((res)=>{return res.json()})
     .then((data)=>{
         console.log(data);
         str=``
-        data.map((post)=>{
-            str+=`<li>${post.id}-></li>`
+        data.map((todo)=>{
+            str+=`<li>${todo.id}---${todo.completed}-------------${todo.title}</li>`
         })
         document.getElementById("list").innerHTML=str
     })
