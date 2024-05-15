f9507e548dd145e762a73424d74c21b339539105
kiranbinoy <kiranbinoy257@gmail.com>
2024-05-15 12:17:17 +0530

    email

--- email regexp/custom.js
+++ email regexp/custom.js
@@ -1,6 +1,43 @@
+
 document.getElementById("email").addEventListener('keyup',(e)=>{
     console.log(e.target.value);
-    let str=/(.^[.*a-z]|[A-Z]|)$/
-    let x= str.test(e.target.value)
+    let regx=/^([a-z A-Z]+|)([0-9 \._]+)@([a-z A-Z 0-9])+.([a-z]+)(.[a-z]+)?$/
+    let x= regx.test(e.target.value)
     console.log(x);
+    if(x){
+        document.getElementById("res").innerHTML="Sucessfull"
+    }
+    else{
+        document.getElementById("res").innerHTML="Inavalid Email id"
+
+    }
+})
+
+document.getElementById("userid").addEventListener('keyup',(e)=>{
+    console.log(e.target.value);
+    let regx=/^([a-z A-Z]+|)([0-9 \.]+)([a-z A-Z 0-9])+.([a-z]+)(.[a-z]+)?$/
+    let x= regx.test(e.target.value)
+    console.log(x);
+    if(x){
+        document.getElementById("user").innerHTML="Sucessfull"
+    }
+    else{
+        document.getElementById("user").innerHTML="Inavalid User Id "
+
+    }
+})
+
+
+document.getElementById("password").addEventListener('keyup',(e)=>{
+    console.log(e.target.value);
+    let regx=/^([a-z A-Z]+|)([0-9 \.]+)([a-z A-Z 0-9])+.([a-z]+)(.[a-z]+)?$/
+    let x= regx.test(e.target.value)
+    console.log(x);
+    if(x){
+        document.getElementById("pass").innerHTML="Sucessfull"
+    }
+    else{
+        document.getElementById("pass").innerHTML="Inavalid Password"
+
+    }
 })
